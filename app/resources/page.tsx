1c65e05a8078843798910f5d014bc6e1e0ec963d
yousophia <yous4@rpi.edu>
2024-09-01 00:18:06 -0400

    start of web development section

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -8,30 +8,23 @@ import Image from "next/image";
 
 export default function page() {
 	return (
-		<div className="flex flex-col w-full h-fit min-h-screen items-center justify-center">
+		<><div className="flex flex-col w-full h-fit min-h-screen items-center justify-center">
 			<NavBar showOnScroll={false} />
 			<div className="flex flex-col w-11/12 md:w-1/2 flex-grow flex-shrink basis-auto items-center justify-center text-center desktop:text-left">
-				<div className="h-24 desktop:h-16"></div>
-				<Image src={logo} alt="HackRPI Logo" width={100} height={100} />
-				<h1 className="text-4xl font-bold mb-4">🚧 HackRPI Site Under Development! 🚧</h1>
-				<p className="text-xl mb-2">
-					Attention all code people and caffeine aficionados! Our hackathon hub is currently in the middle of an epic
-					upgrade. We&apos;re coding faster than a team of over-caffeinated developers at 3 AM.
-				</p>
-				<ul className="list-disc w-full list-inside mb-2">
-					<li>Challenges so brain-bending, they&apos;ll make your algorithms sweat!</li>
-					<li>Prizes so cool, even your keyboard will do a happy dance.</li>
-					<li>Resources so valuable, they&apos;ll make Stack Overflow blush.</li>
-				</ul>
-				<p className="mb-2">
-					Stay tuned for the ultimate hackathon experience. In the meantime, feel free to practice your speed coding and
-					stock up on energy drinks. This is going to be one heck of a digital showdown.
-				</p>
-				<p className="w-full mb-2">
-					Thanks for your patience! (And for not accidentally deploying anything to production.)
+				<h1 className="text-4xl font-bold mb-4">Don&apos;t know where to start?</h1>
+				<p className="text-xl">
+					Do not worry! Check out the tutorials below to help you get on track.
 				</p>
 			</div>
-			<Footer />
 		</div>
+		<div className="text-left mb-5 ml-56 mr-56">
+				<h2 className="text-4xl text-white font-bold">Web Development</h2>
+				<p className="w-11/12 desktop:w-full">
+				Web development involves a wide array of technologies, each serving different purposes, 
+				from front-end design to back-end processing, database management, and deployment. 
+				Here's an overview of the key technologies used in web development:
+				</p>
+			</div>
+		<Footer/></>
 	);
 }
