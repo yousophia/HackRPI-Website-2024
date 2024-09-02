8874bc2c66e355bd9b42028298d5e50d1be637ef
yousophia <yous4@rpi.edu>
2024-09-02 00:24:22 -0400

    some technologies on web wo bulletts

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -8,12 +8,12 @@ import Image from "next/image";
 
 export default function page() {
 	return (
-		<><div className="flex flex-col w-full h-fit min-h-screen items-center justify-center">
+		<><div className="flex flex-col w-full h-56 items-center justify-center">
 			<NavBar showOnScroll={false} />
 			<div className="flex flex-col w-11/12 md:w-1/2 flex-grow flex-shrink basis-auto items-center justify-center text-center desktop:text-left">
-				<h1 className="text-4xl font-bold mb-4">Don&apos;t know where to start?</h1>
+				<h1 className="text-4xl font-bold">Don&apos;t know where to start?</h1>
 				<p className="text-xl">
-					Do not worry! Check out the tutorials below to help you get on track.
+					Check out the tutorials below to help you get hacking.
 				</p>
 			</div>
 		</div>
@@ -22,8 +22,14 @@ export default function page() {
 				<p className="w-11/12 desktop:w-full">
 				Web development involves a wide array of technologies, each serving different purposes, 
 				from front-end design to back-end processing, database management, and deployment. 
-				Here's an overview of the key technologies used in web development:
+				Here&apos;s an overview of the key technologies used:
 				</p>
+				<ul>
+					<li>HTML: The foundational language for structuring content on the web</li>
+					<li>CSS: Used to style and layout web pages, controlling the look and feel</li>
+					<li>JavaScript: A programming language that enables interactive features like animations, form validation, and dynamic content updates</li>
+					<li>React: A JavaScript library for building user interfaces, particularly single-page applications</li>
+				</ul>
 			</div>
 		<Footer/></>
 	);
