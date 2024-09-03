37532eee2dfe29f65647b125d32144ada3219ed7
yousophia <yous4@rpi.edu>
2024-09-02 23:31:03 -0400

    web dev w bullets

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -11,24 +11,24 @@ export default function page() {
 		<><div className="flex flex-col w-full h-56 items-center justify-center">
 			<NavBar showOnScroll={false} />
 			<div className="flex flex-col w-11/12 md:w-1/2 flex-grow flex-shrink basis-auto items-center justify-center text-center desktop:text-left">
-				<h1 className="text-4xl font-bold">Don&apos;t know where to start?</h1>
-				<p className="text-xl">
-					Check out the tutorials below to help you get hacking.
+				<h1 className="flex text-5xl font-bold">Don&apos;t know where to start?</h1>
+				<p className="flex text-xl">
+					Check out the tutorials &amp; key technologies used for each type of development below to help you get hacking.
 				</p>
 			</div>
 		</div>
 		<div className="text-left mb-5 ml-56 mr-56">
 				<h2 className="text-4xl text-white font-bold">Web Development</h2>
-				<p className="w-11/12 desktop:w-full">
-				Web development involves a wide array of technologies, each serving different purposes, 
-				from front-end design to back-end processing, database management, and deployment. 
-				Here&apos;s an overview of the key technologies used:
-				</p>
-				<ul>
-					<li>HTML: The foundational language for structuring content on the web</li>
-					<li>CSS: Used to style and layout web pages, controlling the look and feel</li>
-					<li>JavaScript: A programming language that enables interactive features like animations, form validation, and dynamic content updates</li>
-					<li>React: A JavaScript library for building user interfaces, particularly single-page applications</li>
+				<ul className="list-disc w-full list-inside mb-2">
+					<li>Challenges so brain-bending, they&apos;ll make your algorithms sweat!</li>
+					<li>Prizes so cool, even your keyboard will do a happy dance.</li>
+					<li>Resources so valuable, they&apos;ll make Stack Overflow blush.</li>
+				</ul>
+				<h3 className="text-4xl text-white font-bold">Mobile Development</h3>
+				<ul className="list-disc w-full list-inside mb-2">
+					<li>Challenges so brain-bending, they&apos;ll make your algorithms sweat!</li>
+					<li>Prizes so cool, even your keyboard will do a happy dance.</li>
+					<li>Resources so valuable, they&apos;ll make Stack Overflow blush.</li>
 				</ul>
 			</div>
 		<Footer/></>
